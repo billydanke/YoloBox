f52153e107b9a13801bfce198a9664757286be42
billydanke <johnjo490@gmail.com>
2025-05-12 21:27:41 -0500

    added virtualzing scrollable list for data validation images

---
+++ YoloBox/Converters/BooleanToRedGreenColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace YoloBox.Converters
+{
+    public class BooleanToRedGreenColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? Brushes.LimeGreen : Brushes.Red;
+            }
+            return Brushes.Transparent; // fallback if value is not bool
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- YoloBox/Models/DataCleaningImage.cs
+++ YoloBox/Models/DataCleaningImage.cs
@@ -1,23 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using System.Windows.Media.Imaging;
+using YoloBox.Classes;
 
 namespace YoloBox.Models
 {
-    public class DataCleaningImage
+    public class DataCleaningImage : INotifyPropertyChanged
     {
         public string ImagePath { get; set; }
-        public bool IsIncluded { get; set; }
-        public bool IsSeen { get; set; }
+
+        private bool _isIncluded;
+        public bool IsIncluded
+        {
+            get => _isIncluded;
+            set
+            {
+                if (_isIncluded != value)
+                {
+                    _isIncluded = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private bool _isSeen;
+        public bool IsSeen
+        {
+            get => _isSeen;
+            set
+            {
+                if(_isSeen != value)
+                {
+                    _isSeen = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand ToggleIncludedCommand { get; }
 
         public DataCleaningImage(string imagePath, bool isIncluded = true, bool isSeen = false)
         {
             ImagePath = imagePath;
             IsIncluded = isIncluded;
             IsSeen = isSeen;
+
+            ToggleIncludedCommand = new RelayCommand(_ => IsIncluded = !IsIncluded);
         }
 
         public BitmapImage Image
@@ -28,11 +62,15 @@ namespace YoloBox.Models
                 bitmap.BeginInit();
                 bitmap.CacheOption = BitmapCacheOption.OnLoad;
                 bitmap.UriSource = new Uri(ImagePath);
-                bitmap.DecodePixelWidth = 256;
+                bitmap.DecodePixelWidth = 640;
                 bitmap.EndInit();
                 bitmap.Freeze();
                 return bitmap;
             }
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
